eade86cd77cf2d7410a2512de130d2d3542601c6
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-03-30 15:27:20 +0300

    new api

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import { Videos} from './';
-import  ChannelCard from './ChanneCard';
+import {Videos} from "./";
+import ChannelCard from "./ChanneCard";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
@@ -31,7 +31,7 @@ const ChannelDetail = () => {
       <Box>
         <div style={{
           height:'300px',
-          background: 'linear-gradient(90deg, rgba(0, 128, 255, 1) 0%, rgba(0, 85, 255, 1) 100%)',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
           zIndex: 10,
         }} />
         <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
